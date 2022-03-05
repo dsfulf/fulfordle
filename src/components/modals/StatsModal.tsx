a0a054ea7d9f2ae57d0298e80019d549c98f2afe
David S. Fulford <dsfulford@gmail.com>
2022-03-05 11:04:23 -0600

    add campaign links to modals
    Signed-off-by: David S. Fulford <dsfulford@gmail.com>

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -89,25 +89,27 @@ export const StatsModal = ({
       )}
 
       <div>
-        <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">{' '}
-        <a
-          href="https://www.fulfordforcongress.com"
-          target="_blank"
-          className="underline font-bold"
+        <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+          {' '}
+          <a
+            href="https://www.fulfordforcongress.com"
+            target="_blank"
+            className="underline font-bold"
           >
-        Learn all about me here!
-        </a>{' '}
-      </p>
+            Learn all about me here!
+          </a>{' '}
+        </p>
 
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">{' '}
-        <a
-          href="https://secure.actblue.com/donate/robin-for-congress-1"
-          target="_blank"
-          className="underline font-bold"
+        <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+          {' '}
+          <a
+            href="https://secure.actblue.com/donate/robin-for-congress-1"
+            target="_blank"
+            className="underline font-bold"
           >
-        Support my campaign by donating here!
-        </a>{' '}
-      </p>
+            Support my campaign by donating here!
+          </a>{' '}
+        </p>
       </div>
     </BaseModal>
   )
