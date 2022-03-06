d3e607ec60b4a4e948817f1190d88faa64b515c4
David S. Fulford <dsfulford@gmail.com>
2022-03-06 15:17:36 -0600

    add additional links
    Signed-off-by: David S. Fulford <dsfulford@gmail.com>

--- src/components/campaign/CampaignLinks.tsx
+++ src/components/campaign/CampaignLinks.tsx
@@ -2,7 +2,66 @@ export const CampaignLinks = () => {
   return (
     <div>
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        {' '}
+        <span style={{ fontStyle: 'normal' }} className="font-bold">
+          Support these transgender advocacy groups:
+        </span>
+        <br />
+        <a
+          href="https://secure.everyaction.com/Aluze8fLdEmlU5KgX5Ar1w2"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          Transgender Education Network of Texas
+        </a>
+        <br />
+        <a
+          href="https://www.equalitytexas.org/take-action/donate/give/"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          Equality Texas
+        </a>
+        <br />
+        <a
+          href="https://www.latinatranstexas.org/done"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          Organización Latina de Trans en Texas
+        </a>
+        <br />
+        <a
+          href="https://blacktrans.org/donate/"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          Black Trans Advocacy Coalition
+        </a>
+        <br />
+        <a
+          href="https://www.aclutx.org/en/donating-aclu-general-information/"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          ACLU of Texas
+        </a>
+        <br />
+        <a
+          href="https://support.lambdalegal.org/site/SPageNavigator/donateApplePay.html"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          Lambda Legal
+        </a>
+      </p>
+
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         <a
           href="https://www.fulfordforcongress.com"
           target="_blank"
@@ -10,11 +69,10 @@ export const CampaignLinks = () => {
           className="underline font-bold"
         >
           Learn all about me here!
-        </a>{' '}
+        </a>
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        {' '}
         <a
           href="https://secure.actblue.com/donate/robin-for-congress-1"
           target="_blank"
@@ -22,7 +80,7 @@ export const CampaignLinks = () => {
           className="underline font-bold"
         >
           Support my campaign by donating here!
-        </a>{' '}
+        </a>
       </p>
     </div>
   )
