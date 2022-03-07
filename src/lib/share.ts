8657aba1dad9f48d765116c4d3c45cd724804f7c
David S. Fulford <dsfulford@gmail.com>
2022-03-06 21:09:42 -0600

    include link in share
    Signed-off-by: David S. Fulford <dsfulford@gmail.com>

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -21,7 +21,8 @@ export const shareStatus = (
     `${GAME_TITLE} ${solutionIndex} ${
       lost ? 'X' : guesses.length
     }/${MAX_CHALLENGES}${isHardMode ? '*' : ''}\n\n` +
-    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode))
+    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode)) + '\n\n' +
+    'Play @ https://fulfordle.vercel.app'
 
   const shareData = { text: textToShare }
 
